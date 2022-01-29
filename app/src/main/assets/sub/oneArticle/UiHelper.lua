92da4168496292124f1e8c6297ccd6937b57d9c0
StarVase <lxz2102141297@163.com>
2022-01-29 21:19:21 +0800

    tab

--- app/src/main/assets/sub/oneArticle/UiHelper.lua
+++ app/src/main/assets/sub/oneArticle/UiHelper.lua
@@ -44,3 +44,61 @@ scroll.setOnScrollChangeListener({
 })
 
 --tablay.addTab()
+
+function addCard(text,src,gb)
+  tablay.addView(loadlayout({
+    LinearLayout;
+    layout_height="48dp";
+    gravity="center";
+    {
+      CardView;
+      radius="16dp";
+      layout_margin="8dp";
+      layout_height="32dp";
+      elevation="0dp";
+      BackgroundColor=0;
+
+      onClick=gb,
+      {
+        LinearLayout;
+        gravity="center";
+        layout_height="fill";
+        orientation="horizontal",
+        id="mytab";
+        {
+          AppCompatImageView;
+          layout_height="22dp";
+          layout_marginRight="1dp";
+          ImageResource=src,
+          layout_marginLeft="0dp";
+          ColorFilter=icon,
+        };
+
+        {
+          AppCompatTextView;
+          layout_marginRight="16dp";
+          text=text;
+          layout_marginLeft="1dp";
+          textColor=textColor,
+        };
+      };
+    };
+  }))
+  --graph.ButtonFrame(mytab,math.dp2px(1),icon,0,math.dp2px(16))
+  --pcall(function()mytab.foreground=graph.Ripple(nil,普通波纹)end)
+end
+
+addCard("朗读",R.drawable.ic_text_to_speech,
+function()import "android.speech.tts.*"
+  if mTextSpeech then
+    mTextSpeech.speak((artcon), TextToSpeech.QUEUE_FLUSH, nil);
+   else
+    mTextSpeech = TextToSpeech(activity, TextToSpeech.OnInitListener{
+      onInit=function(status)
+        mTextSpeech.setPitch(1);
+        mTextSpeech.setSpeechRate(1);
+        mTextSpeech.speak((artcon), TextToSpeech.QUEUE_FLUSH, nil);
+      end
+    });
+  end
+end)
